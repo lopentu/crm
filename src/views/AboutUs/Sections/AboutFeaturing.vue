43a3686c1690bf51a6aaf7630728845c55339cfe
teewhy <tyleryeh047@gmail.com>
2025-06-17 19:31:10 +0800

    ref: move AboutUs to views directory

--- src/views/AboutUs/Sections/AboutFeaturing.vue
+++ src/views/AboutUs/Sections/AboutFeaturing.vue
@@ -1,6 +1,6 @@
 <script setup>
 // example component
-import DefaultCounterCard from "../../../../examples/cards/counterCards/DefaultCounterCard.vue";
+import DefaultCounterCard from "../../../examples/cards/counterCards/DefaultCounterCard.vue";
 </script>
 <template>
   <section class="pt-4 pb-6" id="count-stats">
@@ -10,43 +10,37 @@ import DefaultCounterCard from "../../../../examples/cards/counterCards/DefaultC
           <img
             class="w-100 opacity-7"
             src="assets/img/logos/gray-logos/logo-coinbase.svg"
-            alt="可放語奧相關的標誌etc"
-          />
+            alt="可放語奧相關的標誌etc" />
         </div>
         <div class="col-lg-2 col-md-4 col-6 mb-4">
           <img
             class="w-100 opacity-7"
             src="@/assets/img/logos/gray-logos/logo-nasa.svg"
-            alt="logo"
-          />
+            alt="logo" />
         </div>
         <div class="col-lg-2 col-md-4 col-6 mb-4">
           <img
             class="w-100 opacity-7"
             src="@/assets/img/logos/gray-logos/logo-netflix.svg"
-            alt="logo"
-          />
+            alt="logo" />
         </div>
         <div class="col-lg-2 col-md-4 col-6 mb-4">
           <img
             class="w-100 opacity-7"
             src="@/assets/img/logos/gray-logos/logo-pinterest.svg"
-            alt="logo"
-          />
+            alt="logo" />
         </div>
         <div class="col-lg-2 col-md-4 col-6 mb-4">
           <img
             class="w-100 opacity-7"
             src="@/assets/img/logos/gray-logos/logo-spotify.svg"
-            alt="logo"
-          />
+            alt="logo" />
         </div>
         <div class="col-lg-2 col-md-4 col-6 mb-4">
           <img
             class="w-100 opacity-7"
             src="@/assets/img/logos/gray-logos/logo-vodafone.svg"
-            alt="logo"
-          />
+            alt="logo" />
         </div>
       </div>
       <div class="row justify-content-center text-center">
@@ -55,8 +49,7 @@ import DefaultCounterCard from "../../../../examples/cards/counterCards/DefaultC
             title="研究成果"
             description="Of “high-performing” level are led by a certified project manager"
             :count="5234"
-            :duration="3000"
-          />
+            :duration="3000" />
         </div>
         <div class="col-md-3">
           <DefaultCounterCard
@@ -64,8 +57,7 @@ import DefaultCounterCard from "../../../../examples/cards/counterCards/DefaultC
             description="That meets quality standards required by our users"
             :count="3400"
             suffix="+"
-            :duration="3000"
-          />
+            :duration="3000" />
         </div>
         <div class="col-md-3">
           <DefaultCounterCard
@@ -73,8 +65,7 @@ import DefaultCounterCard from "../../../../examples/cards/counterCards/DefaultC
             description="Actively engage team members that finishes on time"
             :count="24"
             suffix="/7"
-            :duration="4000"
-          />
+            :duration="4000" />
         </div>
       </div>
     </div>
--- src/views/AboutUs/Sections/AboutInformation.vue
+++ src/views/AboutUs/Sections/AboutInformation.vue
@@ -1,7 +1,7 @@
 <script setup>
 // example components
-import DefaultInfoCard from "../../../../examples/cards/infoCards/DefaultInfoCard.vue";
-import CenteredBlogCard from "../../../../examples/cards/blogCards/CenteredBlogCard.vue";
+import DefaultInfoCard from "../../../examples/cards/infoCards/DefaultInfoCard.vue";
+import CenteredBlogCard from "../../../examples/cards/blogCards/CenteredBlogCard.vue";
 </script>
 <template>
   <section class="py-7">
@@ -13,36 +13,31 @@ import CenteredBlogCard from "../../../../examples/cards/blogCards/CenteredBlogC
               color="info"
               icon="public"
               title="Goal"
-              description="1"
-            />
+              description="1" />
             <DefaultInfoCard
               color="info"
               icon="payments"
               title="Payments functionality"
-              description="2"
-            />
+              description="2" />
           </div>
           <div class="row justify-content-start mt-4">
             <DefaultInfoCard
               color="info"
               icon="apps"
               title="Prebuilt components"
-              description="3"
-            />
+              description="3" />
             <DefaultInfoCard
               color="info"
               icon="3p"
               title="Improved platform"
-              description="4"
-            />
+              description="4" />
           </div>
         </div>
         <div class="col-lg-4 ms-auto mt-lg-0 mt-6">
           <CenteredBlogCard
             image="https://images.unsplash.com/photo-1544717302-de2939b7ef71?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
             title="謝舒凱 教授"
-            description="經驗/履歷"
-          />
+            description="經驗/履歷" />
         </div>
       </div>
     </div>
