cd8f92d52e10bfd2682fd46f3322bf18396db006
justinchen <c.hung-chi@wustl.edu>
2025-06-24 16:32:17 +0800

    Change word color

--- src/views/Presentation/Sections/PresentationTestimonials.vue
+++ src/views/Presentation/Sections/PresentationTestimonials.vue
@@ -1,5 +1,5 @@
 <script setup>
-import DefaultReviewCard from "@/examples/cards/reviewCards/DefaultReviewCard.vue";
+//import DefaultReviewCard from "@/examples/cards/reviewCards/DefaultReviewCard.vue";
 </script>
 <template>
   <section class="py-7">
@@ -7,7 +7,7 @@ import DefaultReviewCard from "@/examples/cards/reviewCards/DefaultReviewCard.vu
       <div class="row">
         <div class="col-lg-6 mx-auto text-center">
           <h2 class="mb-0">A placeholder for</h2>
-          <h2 class="text-gradient text-success mb-3">
+          <h2 class="text-gradient text-info mb-3">
             some really cool statistics
           </h2>
           <p class="lead">And some really cool captions for the results.</p>
